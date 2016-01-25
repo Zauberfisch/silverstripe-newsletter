5085053c14e78ae1c18aa8967d55b0fe1c8ffc35
zauberfisch <admin@zauberfisch.at>
2016-01-25 06:41:45 +0000

    Newsletter MessageQueue no auto retrigger by queue because the Newsletter Admin triggers it

--- _config.php
+++ _config.php
@@ -14,7 +14,7 @@ if (class_exists('MessageQueue')) {
 		"encoding" => "php_serialize",
 		"send" => array( "onShutdown" => "all" ),
 		"delivery" => array( "onerror" => array( "log" ) ),
-		"retrigger" => "yes", // on consume, retrigger if there are more items
+		"retrigger" => "no",
 		"onShutdownMessageLimit" => "1" // one message per async process
 	));
 }
